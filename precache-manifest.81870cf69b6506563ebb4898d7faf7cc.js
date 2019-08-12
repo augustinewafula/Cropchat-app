99a9be642ff16a439a98021f8d0f6495521ee2a9
AugustineTreezy <augustinetreezy@outlook.com>
2019-08-12 14:41:57 +0300

    deploy

---
+++ precache-manifest.81870cf69b6506563ebb4898d7faf7cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "eff37f99900e0b6b752e",
+    "url": "/Cropchat-app/js/about.7969822e.js"
+  },
+  {
+    "revision": "ab796bcc2448a8033d1f",
+    "url": "/Cropchat-app/css/app.975d998a.css"
+  },
+  {
+    "revision": "ab796bcc2448a8033d1f",
+    "url": "/Cropchat-app/js/app.45e14619.js"
+  },
+  {
+    "revision": "4e87075c051fbf744caa",
+    "url": "/Cropchat-app/css/chunk-vendors.aeb03eba.css"
+  },
+  {
+    "revision": "4e87075c051fbf744caa",
+    "url": "/Cropchat-app/js/chunk-vendors.6707ded5.js"
+  },
+  {
+    "revision": "4a88b46110b0606823f2",
+    "url": "/Cropchat-app/js/detail.61f43db0.js"
+  },
+  {
+    "revision": "88fa5f9cf9910bf942cc",
+    "url": "/Cropchat-app/css/post.66b42bbb.css"
+  },
+  {
+    "revision": "88fa5f9cf9910bf942cc",
+    "url": "/Cropchat-app/js/post.e016439e.js"
+  },
+  {
+    "revision": "c5eb2a9dcfeb4d50ba0aded1738d8e55",
+    "url": "/Cropchat-app/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Cropchat-app/robots.txt"
+  },
+  {
+    "revision": "fbdd6ac5a056e7c1b2132a438151f090",
+    "url": "/Cropchat-app/img/users/user.png"
+  }
+];
